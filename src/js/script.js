b4af195f5e0f0f0587429157267946c2e4382e0d
Sterio <sterio.zonios@hotmail.com>
2019-09-30 15:30:49 +0200

    refait design homepage

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,17 @@
+$(".main").css("padding-top", $(".mainNav").css("height"));
+
 // Navbar transition
 $(window).on('scroll', function () {
   let nav = $('.mainNav');
+  let navHeight = parseFloat(nav.css("height"));
   let windowTop = $(window).scrollTop();
-  if ($("header").height() < windowTop) {
+  if (4 * navHeight < windowTop) {
     nav.addClass('dynamicNav');
     nav.addClass('fixedNav');
-  } else if (nav.height() + 20 < windowTop) {
+  } else if ( navHeight + 10 < windowTop) {
     nav.addClass('dynamicNav');
     nav.removeClass('fixedNav');
+    console.log(nav.css("height"));
   } else {
     nav.removeClass('dynamicNav');
     nav.removeClass('fixedNav');
