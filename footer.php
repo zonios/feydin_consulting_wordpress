ba918fba32744f3e37f8dbcd7422598d4481a28d
Sterio <sterio.zonios@hotmail.com>
2019-10-01 10:02:28 +0200

    rajout du footer

--- footer.php
+++ footer.php
@@ -1,6 +1,29 @@
     
  
   </div>
+  <footer class="mainFooter">
+    <div class="footerTabs">
+      <div>
+        <h2>Liens pour le site</h2>
+        <p><a href="">Overview</a></p>
+        <p><a href="">Sales Performances</a></p>
+        <p><a href="">Our Insights</a></p>
+      </div>
+      <div>
+        <h2>Dernières nouvelles</h2>
+        <p><a href="">exemples de nouvelles intéressantes</a></p>
+        <p><a href="">exemples de nouvelles intéressantes</a></p>
+        <p><a href="">exemples de nouvelles intéressantes</a></p>
+      </div>
+      <div>
+        <h2>Contactez-nous</h2>
+        <p>Notre adresse</p>
+        <p>+32 0123 54 87 23</p>
+        <p>exemple d'info intéressante</p>
+      </div>
+    </div>
+    <p>© FEYDIN MANAGEMENT CONSULTING</p>
+  </footer>
   <?php wp_footer(); ?>
 </body>
 
