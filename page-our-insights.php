405b33c9182704a4c376bb1cd45aac34caf8716f
Sterio <sterio.zonios@hotmail.com>
2019-10-02 10:48:22 +0200

    petit ajout placeholder dans page insights

--- page-our-insights.php
+++ page-our-insights.php
@@ -8,7 +8,32 @@ get_header();
     <source src="<?= get_template_directory_uri(); ?>/src/images/our_insight_video.mp4" type="video/mp4">
   </video>
 </div>
-
+<div class="insightContent">
+  <h2>
+    Marketing anarchy in the iPhone range?
+  </h2>
+  <p>
+    To introduce this article please let me give you an insight into the Apple commercial results: At the end of 2017, iPhone sales accounted for 51% of global smartphone revenues (61.4 billions dollars vs 120 billions estimated total). So we can easily consider this product as an ultra strategic cash maker for Apple.
+    <br><br>
+    <i>But last year the Apple Marketing team threw sand in the gears. And I was expecting some issues regarding the perceived logic of the Apple iPhone portfolio since the launch of the model X.</i>
+    <br><br>
+    At this time, I was very surprised by the launch of the iPhone 9 and 10 at the same time. But also by the fact that they have decided to change the nomenclature of their flagship choosing a letter instead of an Arabic numeral. Could you imagine BMW developing their série 1,2,3,4,5,6 and launching the flagship série VII? I believe that this should be incompatible with a logical German engineer mind, right? It was obviously not a blocking point in Cupertino!
+    <br><br>
+    First consequence is that some journalists and customers pronounced the letter X instead of saying 10. Second negative consequence, the iPhone 9 was not considered as a sexy and performing smartphone but rather as the "poor man" iPhoneX. This confusion was reinforced by the fact that Apple keeps now oldest models longer in distribution.
+    <br><br>
+    By the way this extended range is for me a good thing in order to propose to consumers a wider price proposal but also secure their investment on a longer term.
+    <br><br>
+    The iPhone 9 was consequently not a commercial success. This is proven that the 9 simply disappeared from the range creating a new hole in the range between the 8 and the X. This hole looks like a dangerous imaginary frontier between the old and the new models of the portfolio.
+    <br><br>
+    <i>This hole should be fulfilled by the iPhone Xr which is more or less presented as a smaller and less powerful iPhone Xs. And now it is becoming once again counter intuitive considering that everywhere in the World, the two letters X and S associated means in consumer's minds: eXtra Small. This is the opposite of the flagship smartphone main characteristic! </i>
+        <br><br>
+        Rumors before the keynote announced that the naming could be Xmax and Xc.Doing so, Xc could be associated with "compact". This was much more logical in my opinion, the ex 9 being smaller (5.2") than the previous X (5.8").
+        <br><br>
+        Despite these Marketing mistakes, I do not worry too much for Apple as they are making outstanding products with a lot of credibility and appeal on the Electronic high-end Market.
+        <br><br>
+        I am a fan of this 1 trillion $ company so you will understand that I expect perfection from them...even regarding the product nomenclature.
+  </p>
+</div>
 <?php
 get_footer();
 ?>
